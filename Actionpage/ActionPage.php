59d5082e7c4713e3d9ea9e4bf7b3aff78b8b59bc
Brunozhon <69879040+Brunozhon@users.noreply.github.com>
2020-08-20 13:58:00 -0400

    Action Page
    Used for returning input from other HTML files.

---
+++ Actionpage/ActionPage.php
@@ -0,0 +1,4 @@
+echo "Your input was recived as: ";
+echo htmlspecialchars($_SERVER["PHP_SELF"]);
+
+echo "The server has returned and proccesed your input using 'htmlspecialchars()' ans returned that.";
